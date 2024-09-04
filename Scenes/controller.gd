08e540cb89c6cf4f4a5078089484c4b0b5491b46
Mat Hawley <mathewjonhawley@gmail.com>
2024-09-04 12:33:25 -0400

    made so tris auto populate

--- Scenes/controller.gd
+++ Scenes/controller.gd
@@ -1,4 +1,4 @@
-extends MeshInstance3D
+extends CharacterBody3D
 
 const ACCEL = 10
 const DEACCEL = 30
@@ -6,25 +6,48 @@ const DEACCEL = 30
 const SPEED = 5.0
 const SPRINT_MULT = 2
 const JUMP_VELOCITY = 4.5
-
+var bone_length :float = 0
+var bone_numbers :int = 0
 @onready var body_piece_one :MeshInstance3D = get_node("../body1")
 @onready var body_piece_two :MeshInstance3D = get_node("../body2")
+@onready var nav : NavigationAgent3D = $NavigationAgent3D
+@onready var target :Marker3D = get_node("../Marker3D")
 var body_segment_pimitived :Array[MeshInstance3D] = []
 
+@onready var skeleton :Skeleton3D = get_node("../snake_hefty/Armature/Skeleton3D")
+var tris_ready :bool = false
+
 func _ready() -> void:
-	
-	# get all body segments into an array . 
-	
-	var all_nodes :Array[Node] = get_node("..").get_children()
-	for i in range(all_nodes.size()):
-		if all_nodes[i].name.contains("body"):
-			body_segment_pimitived.append(all_nodes[i])
+		# get bone lenghts 
+	bone_length = calc_length()
+	print("bone length ", skeleton.get_bone_count())
+	bone_numbers = skeleton.get_bone_count()
+		# get # of bones 
 		
-	for i in range(body_segment_pimitived.size()):
-		print(body_segment_pimitived[i].name, " ")
+	# build as manny triangles as you have bones . 
+	var tri_array :Array[MeshInstance3D] 
+	
+	for i in bone_numbers:
+		tri_array.append(MeshInstance3D.new())
+		tri_array[i].mesh = PrismMesh.new()
+		tri_array[i].name = "body"+str(i)
+		tri_array[i].global_position = Vector3(0,0,i*.2)
+		self.add_sibling.call_deferred(tri_array[i])
 
 
 func _process(delta: float) -> void:
+	
+	if tris_ready != true:
+	# get all body segments into an array .
+		var all_nodes :Array[Node] = get_node("..").get_children()
+		for i in range(all_nodes.size()):
+			if all_nodes[i].name.contains("body"):
+				body_segment_pimitived.append(all_nodes[i])
+			
+		for i in range(body_segment_pimitived.size()):
+			print(body_segment_pimitived[i].name, " ")
+		tris_ready = true
+		 
 	
 	
 	# This just controls acceleration. Don't touch it.
@@ -32,9 +55,19 @@ func _process(delta: float) -> void:
 	
 	var input_dir :Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
-	position += Vector3(input_dir.x*delta*SPEED,0,input_dir.y*delta*SPEED)
+	target.global_position += Vector3(input_dir.x*delta*SPEED,0,input_dir.y*delta*SPEED)
 	
 	follower(delta)
+	
+func _physics_process(delta: float) -> void:
+	
+	var current_location = global_transform.origin
+	nav.target_position = target.global_position
+	var next_location = nav.get_next_path_position()
+	var new_velocity = (next_location - current_location).normalized() * SPEED
+	
+	velocity = new_velocity
+	move_and_slide()
 	
 
 func follower(delta):
@@ -42,13 +75,17 @@ func follower(delta):
 		
 		if i == 1: # meaning its the first piece  
 			body_segment_pimitived[i].look_at(global_position)
-			if ((global_position - body_segment_pimitived[i].global_position).length() > 1):
+			if ((global_position - body_segment_pimitived[i].global_position).length() > bone_length):
 				body_segment_pimitived[i].global_position += (global_position - body_segment_pimitived[i].global_position) * delta * SPEED
-			if ((global_position - body_segment_pimitived[i].global_position).length() < 1.1):
+			if ((global_position - body_segment_pimitived[i].global_position).length() < bone_length + .1):
 				body_segment_pimitived[i].global_position -= (global_position - body_segment_pimitived[i].global_position) * delta * SPEED
 		else:
 			body_segment_pimitived[i].look_at(body_segment_pimitived[i-1].global_position)
-			if ((body_segment_pimitived[i-1].global_position - body_segment_pimitived[i].global_position).length() > 1):
+			if ((body_segment_pimitived[i-1].global_position - body_segment_pimitived[i].global_position).length() > bone_length):
 				body_segment_pimitived[i].global_position += (body_segment_pimitived[i-1].global_position - body_segment_pimitived[i].global_position) * delta * SPEED
-			if ((body_segment_pimitived[i-1].global_position - body_segment_pimitived[i].global_position).length() < 1.1):
+			if ((body_segment_pimitived[i-1].global_position - body_segment_pimitived[i].global_position).length() < bone_length + .1):
 				body_segment_pimitived[i].global_position -= (body_segment_pimitived[i-1].global_position - body_segment_pimitived[i].global_position) * delta * SPEED
+
+func calc_length():
+	bone_length = (skeleton.get_bone_global_rest(0).origin - skeleton.get_bone_global_rest(1).origin).length()
+	return bone_length
---
+++ Scripts/Snake_new.gd
@@ -0,0 +1,218 @@
+extends Node3D
+
+#Target to be tracked
+@export var target : Node3D
+#How much (in meters) path is expanded each time when needed
+@export var path_update_len : float = 2.0
+#Max speed of snake
+@export var max_speed : float = 0.0
+#Acceleration of snake
+@export var acceleration : float = .5
+#Higher values makes path corners more curved
+@export var path_smoothing : float = 0.2
+
+@onready var path_node : Path3D = $SnakePath
+@onready var movement_path : Curve3D = $SnakePath.curve
+@onready var path_follow : SkeletonPathFollow3D = $SnakePath/SkeletonPathFollow3D
+@onready var nav_agent : NavigationAgent3D = $SnakePath/SkeletonPathFollow3D/steve/NavAgent
+@onready var snake_skin_mesh :MeshInstance3D = $SnakePath/SkeletonPathFollow3D/steve/Armature/Skeleton3D/Cube_001
+var time :float = 0
+var snake_wavyness :float = .3
+var wave_thing :float = 0
+
+var previous_point :Vector3 = Vector3(0,0,0) # used to store the previos point for the wave calculationh
+
+var _nav_map : RID
+@export var _ensnared : bool = false
+signal ensnared 
+
+var snake_state:String = "player_seeking"
+# these are all of the object that I want the snakes to idly move to .
+var patrol_objects :Array = []
+var pick_new_object :bool = false
+
+@export var append_path_hits :int = 0
+
+#Class for easier storage and update of curve points
+class CurvePoint:
+	var point : Vector3
+	var point_in : Vector3
+	var point_out : Vector3
+	
+	func trans(transf : Transform3D):
+		point_in = point + point_in
+		point_out = point + point_out
+		point = transf * point
+		point_in = transf * point_in
+		point_out = transf * point_out
+	
+	func loc(node : Node3D):
+		point = node.to_local(point)
+		point_in = node.to_local(point_in) - point
+		point_out = node.to_local(point_out) - point
+
+func _ready():
+	_nav_map = get_world_3d().get_navigation_map()
+	nav_agent.max_speed = max_speed
+	
+	for child in get_node("../idle_objects").get_children():
+		patrol_objects.append(child)
+	# for getting the speed up on the snakes
+	
+	var reset_snakes_script :Node3D = get_node("..")
+	var callable = Callable(self,"give_snake_speed")
+	
+	reset_snakes_script.connect("restor_speed",callable)
+	# connecting another signal for player 
+	if GlobalVars.game_started == true:
+		give_snake_speed()
+
+
+func _physics_process(delta : float):
+	time += delta
+	wave_thing = (sin(time)*snake_wavyness)
+	
+	
+	if snake_state == "player_seeking":
+		target = get_node("../Player")
+		
+	if snake_state == "patrol":
+		# logic to pick new cub if close enough  .
+		if ((target.global_position - path_follow.global_position).length() < 3.0):
+			pick_new_object = true
+			# this is where random switching and uncontrolled may occur . 
+			
+		if pick_new_object: 
+			# to make sure the next target is different 
+			var next_target = patrol_objects.pick_random()
+			# if the next target is the same keep picking new target
+			while next_target == target:
+				next_target = patrol_objects.pick_random()
+			target = next_target
+			pick_new_object = false
+	#Vector3.RIGHT because that's way snake is facing
+	var targetVel : Vector3 = Vector3.RIGHT * max_speed
+	nav_agent.velocity = nav_agent.velocity.move_toward(targetVel, delta * acceleration)
+	#nav_agent.get_next_path_position()
+	path_follow.progress += nav_agent.velocity.length() * delta
+	if(!_ensnared && movement_path.get_baked_length() - path_follow.progress < path_update_len):
+		_append_path(wave_thing) # now needs time as a argument to make snake do wave
+	if((get_node("../Player").global_position - path_follow.global_position).length() < path_update_len * 1 && !_ensnared):
+		# if it reaches a cube go pick another one
+		
+		_ensnare_target()
+		_ensnared = true
+		
+		emit_signal("ensnared")
+	# check for snake if missed target and need to continue pursuit , IGNORE IF CUBE
+	if (_ensnared and ((target.global_position - path_follow.global_position).length() > 4.0) and ((target.global_position - path_follow.global_position).length() < 16.0)):
+		_ensnared = false
+	# condition to go into patrol mode if too far away from player
+	if !_ensnared and ((get_node("../Player").global_position - path_follow.global_position).length() > 16.0) and not (snake_state == "patrol"):
+		snake_state = "patrol"
+		
+		pick_new_object = true
+		# condition to go into seek player mode 
+	if ((get_node("../Player").global_position - path_follow.global_position).length() < 14) :
+		snake_state = "player_seeking"
+		
+		# condition to seek new patrol target
+	
+	
+
+#Called to prepare next part of path toward target
+func _append_path(wave :float):
+	append_path_hits += 1
+	
+	var lastPathPos : Vector3 = path_node.to_global(movement_path.get_point_position(movement_path.point_count - 1))
+	var currentPos : Vector3 = NavigationServer3D.map_get_closest_point(_nav_map, lastPathPos)
+	var targetPos : Vector3 = NavigationServer3D.map_get_closest_point(_nav_map, target.global_position)
+	var navPath : PackedVector3Array = NavigationServer3D.map_get_path(_nav_map, currentPos, targetPos, true)
+	if(navPath.size() < 2):
+		return
+	var currentPoint : int = 1
+	var lastPos : Vector3 = currentPos
+
+	var lenLeft : float = path_update_len
+	while(lenLeft > 0.0 && currentPoint < navPath.size()):
+		
+		
+		var currentPointPos : Vector3 = navPath[currentPoint]
+		# may want to modify the current PointPos 
+		
+		currentPointPos = Vector3(currentPointPos.x + wave ,currentPointPos.y,currentPointPos.z + wave) # this is a real mouth full but what its doing is gradually moving the points back and forth 
+		#currentPointPos.y = 0.0
+		var clen : float = (currentPointPos - lastPos).length()
+		if(clen > lenLeft):
+			
+			
+			#Too far away
+			var newPoint : Vector3 = lastPos + (currentPointPos - lastPos).normalized() * lenLeft
+			var adding_point :Vector3 = path_node.to_local(newPoint)
+			# get the deirection the snake is heading in
+			var direction_vector :Vector3 = previous_point - adding_point 
+			var sway_point = Perpendicular(direction_vector) 
+			var wavy_point :Vector3 = Vector3(adding_point.x + (sway_point.x * wave) ,adding_point.y,adding_point.z + (sway_point.z * wave))
+			movement_path.add_point(wavy_point)
+			lenLeft = 0.0
+			previous_point = adding_point
+		else:
+			#Too close
+			
+			
+			var adding_point_2 :Vector3 = path_node.to_local(currentPointPos)
+			var direction_vector_2 :Vector3 = previous_point - adding_point_2
+			var sway_point_2 = Perpendicular(direction_vector_2) 
+			
+			var wavy_point_2 :Vector3 = Vector3(adding_point_2.x + (sway_point_2.x * wave) ,adding_point_2.y,adding_point_2.z + (sway_point_2.z * wave))
+			movement_path.add_point(wavy_point_2)
+			previous_point = adding_point_2
+			lenLeft -= clen
+		_smooth_point(path_smoothing)
+		lastPos = currentPointPos
+		currentPoint += 1
+
+		
+		
+
+func _smooth_point(smooth_force : float):
+	var prevPos : Vector3 = movement_path.get_point_position(movement_path.point_count - 3)
+	var nextPos : Vector3 = movement_path.get_point_position(movement_path.point_count - 1)
+	#Just in case
+	#prevPos.y = 0
+	#nextPos.y = 0
+	var handle : Vector3 = (nextPos - prevPos).normalized() * smooth_force
+	movement_path.set_point_out(movement_path.point_count - 2, handle)
+	movement_path.set_point_in(movement_path.point_count - 2, -handle)
+
+func _ensnare_target():
+	var targetTransform : Transform3D = target.global_transform
+	targetTransform = targetTransform.looking_at(path_follow.global_position)
+	var ensData : Curve3D
+	if snake_skin_mesh.mesh.get_faces().size() > 16000:  # more verticies means its the larger snake
+		
+		ensData = preload("res://Resources/EnsnarmentData_large.res")
+	else:
+		ensData = preload("res://Resources/EnsnarmentData.res")
+	#Append to movement path
+	for i in ensData.point_count:
+		var cp : CurvePoint = CurvePoint.new()
+		cp.point = ensData.get_point_position(i)
+		cp.point_in = ensData.get_point_in(i)
+		cp.point_out = ensData.get_point_out(i)
+		cp.trans(targetTransform)
+		cp.loc(path_node)
+		movement_path.add_point(cp.point, cp.point_in, cp.point_out)
+
+#Avoidance is not working anyway...
+func _on_nav_agent_velocity_computed(safe_velocity : Vector3):
+	#nav_agent.velocity = safe_velocity
+	pass
+func give_snake_speed():
+	max_speed = 3
+
+func Perpendicular(input_vect : Vector3):
+	var y_vect :Vector3 = Vector3(0,1,0)
+	var perpendicular_vect = y_vect.cross(input_vect)
+	perpendicular_vect = perpendicular_vect.normalized()
+	return perpendicular_vect
