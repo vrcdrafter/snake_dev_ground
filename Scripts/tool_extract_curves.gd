fef5d6e9c04792fb315889f726b7edbc0e193285
Mat Hawley <mathewjonhawley@gmail.com>
2024-12-06 09:21:14 -0500

    added some logic to the tool extract curves so you can just dump in a snake and extract curves

--- Scripts/tool_extract_curves.gd
+++ Scripts/tool_extract_curves.gd
@@ -1,26 +1,34 @@
 extends Node3D
  # just drop in the stuff 
-@onready var anim_player :AnimationPlayer = get_node("snake_hefty_animated/AnimationPlayer")
-@onready var skeleton_snake :Skeleton3D = get_node("snake_hefty_animated/Armature_001/Skeleton3D")
+var snake_node :Node3D 
+var skeleton :Skeleton3D
+var anim_player :AnimationPlayer
 var one_shot :bool = true
 func _ready() -> void:
+	snake_node = find_child("sn*")
+	var name = snake_node.name
+	skeleton = snake_node.find_child("Skeleton3D",true,true)
+	anim_player = snake_node.find_child("Anim*",true,true)
 	
-	$snake_hefty_animated/AnimationPlayer.play("typing_2_001")
 	
 func _physics_process(delta: float) -> void:
 	if one_shot:
-		_make_curve_from_animation(anim_player,skeleton_snake)
+		_make_curve_from_animation(anim_player,skeleton,false)
 		one_shot = false
 	
-func _make_curve_from_animation(whole_lib :AnimationPlayer, snake_skeleton :Skeleton3D) -> Curve3D:
+func _make_curve_from_animation(whole_lib :AnimationPlayer, snake_skeleton :Skeleton3D, debug :bool):
 	var anim_library :AnimationLibrary = whole_lib.get_animation_library("")
-	whole_lib.play("typing_2_001")
-	whole_lib.advance(0)
-	#whole_lib.seek(0.0,true,false)
-	var curve_new :Curve3D = Curve3D.new()
-	for i in snake_skeleton.get_bone_count():
-		var bone_position :Vector3 = snake_skeleton.get_bone_global_pose(i).origin
-		curve_new.add_point(bone_position)
-	curve_new.resource_name = "transition_curve"
-	var save_result = ResourceSaver.save(curve_new,"res://" + curve_new.resource_name + ".tres")
-	return curve_new
+	var anim_list :Array[StringName] = anim_library.get_animation_list()
+	print(anim_list)
+	if not debug:
+		for i in anim_list.size():
+			whole_lib.play(anim_list[i])
+			whole_lib.advance(0)
+			#whole_lib.seek(0.0,true,false)
+			var curve_new :Curve3D = Curve3D.new()
+			for g in snake_skeleton.get_bone_count():
+				var bone_position :Vector3 = snake_skeleton.get_bone_global_pose(g).origin
+				curve_new.add_point(bone_position)
+			curve_new.resource_name = anim_list[i]
+			var save_result = ResourceSaver.save(curve_new,"res://" + curve_new.resource_name + ".tres")
+	
